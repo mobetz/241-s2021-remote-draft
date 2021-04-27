30ce6b242873328ff531f5cf3a88b370af54b17b
Matthew Obetz <mobetz@massbay.edu>
2021-04-27 19:49:56 -0400

    Instructions for merging

--- src/Main.java
+++ src/Main.java
@@ -115,6 +115,15 @@ public class Main {
 
         Before we do that, we should make sure we save everything we're working on right now to a commit!
 
+
+        In this universe, we never made that second function.
+        (Questions?)
+        What if we wanted to get the best of both worlds, and have all the changes from both branches of the history?
+        We can do that with one final command for today: git merge. Git merging lets us take two different branches, and
+        squash their changes together.
+
+        To merge in IntelliJ, we can right click the other branch we want to merge, and select the merge option.
+
          */
     }
 
